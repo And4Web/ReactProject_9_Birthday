71e5f83203f5780b391b31a69a9634afef4b88d4
And4Web <anands2807@gmail.com>
2022-05-25 10:28:00 +0530

    app created using the data provided.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import List from "./components/List";
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
-      <h1>Birthday app</h1>
+      <section className="container">
+        <h1>{data.length} Birthday app</h1>
+        <List people={data} />
+      </section>
+      <button onClick={() => console.log("clicked")}>Clear all</button>
+      <button onClick={() => console.log("clicked 2nd")}>Add</button>
     </div>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List({ people }) {
+  const { id, name, image, birthday } = people;
+  return (
+    <div>
+      <h2>List componet</h2>
+    </div>
+  );
+}
+
+export default List;
